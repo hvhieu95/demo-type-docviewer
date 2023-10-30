0ce721444992447a1eba6455ca903360d1250ff4
HOANG HIEU <hoanghieubk1995@gmail.com>
2023-10-30 17:00:47 +0900

    first

---
+++ src/documents.tsx
@@ -0,0 +1,38 @@
+
+export type DocumentType = {
+  id: string;
+  name: string;
+  uri: string;
+  fileType: "pdf" | "xlsx" | "docx" | "ppt";
+  assign?:string;
+  status?:string
+};
+
+export const documents: DocumentType[] = [
+  {
+    id: "1",
+    name: "file pdf",
+    uri: "/sample.pdf",
+    fileType:"pdf",
+  },
+  {
+    id: "2",
+    name: " file excel",
+    uri: "https://transfer.sh/c1onBs00w2/%E5%8F%82%E6%95%B0%E8%A1%A8.xlsx",
+    fileType:"xlsx",
+  },
+  {
+    id: "3",
+    name: " file words",
+    uri: "https://transfer.sh/rvq45hMe0N/sample1.docx",
+    fileType:"docx",
+  },
+  {
+    id: "4",
+    name: " file powerpoints",
+    uri: "https://transfer.sh/9hXYSLu39h/file_example_PPT_1MB.ppt",
+    fileType:"ppt",
+  },
+];
+
+export default documents
